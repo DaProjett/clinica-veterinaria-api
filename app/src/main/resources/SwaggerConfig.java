945a7ecbc8bf83b30ba78733776bfca3ab037e3a
Andrea Vargas Loaiza <andreavargasloaiza5@gmail.com>
2026-04-11 09:29:58 -0500

    Update SwaggerConfig.java

--- app/src/main/resources/SwaggerConfig.java
+++ app/src/main/resources/SwaggerConfig.java
@@ -1,1 +1,23 @@
+package com.veterinaria.app.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI veterinariaOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Sistema Veterinario")
+                        .description("Documentación de los endpoints para gestión de citas, dueños y mascotas")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Soporte Técnico")
+                                .email("soporte@veterinaria.com")));
+    }
+}
